c2cb714a7ffd465a4d04c51ba7de5db8e9b72ab3
Cindy1229 <leticiashi3@gmail.com>
2020-06-08 15:42:04 -0400

    Added game over functionality and restart button

--- draw.js
+++ draw.js
@@ -4,7 +4,7 @@ const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 var snake;
-
+var btn;
 
 //Setup function
 (function setup() {
@@ -19,7 +19,7 @@ var snake;
 
 
   //Redraw the canvas every 250 miliseconds
-  window.setInterval(()=>{
+  /*window.setInterval(()=>{
     ctx.clearRect(0,0,canvas.width, canvas.height);
     snake.update();
     snake.draw();
@@ -34,8 +34,42 @@ var snake;
     //Check if snake has collided with itself
     snake.checkCollision();
     document.querySelector('.score').innerText='SCORE: '+snake.total;
-  }, 250)
+  }, 250)*/
+  start();
 }());
+
+//Start the Game
+function start() {
+  //Redraw the canvas every 250 miliseconds
+  var id=window.setInterval(()=>{
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    snake.update();
+    snake.draw();
+    apple.draw();
+
+    //When the snake collides with the Apple
+    if(snake.eat(apple)){
+      //Reset the apple's location
+      apple.pickLocation();
+    }
+
+    //Check if snake has collided with itself
+    if (snake.checkCollision()) {
+      document.querySelector('.score').innerText='Total Score: '+snake.total;
+      document.querySelector('.heading').innerText='Game Over !!';
+      clearInterval(id);
+      btn=document.createElement('button');
+      btn.innerText='Restart';
+      var element=document.querySelector('.btn');
+      element.appendChild(btn);
+
+      btn.addEventListener('click', reset);
+    }
+    else {
+      document.querySelector('.score').innerText='Current Score: '+snake.total;
+    }
+  }, 250)
+}
 
 //Add key listener
 window.addEventListener('keydown', (e)=>{
@@ -43,3 +77,17 @@ window.addEventListener('keydown', (e)=>{
   const direction=e.key.replace('Arrow', '');
   snake.changeDirection(direction);
 });
+
+//Reset the Game
+function reset() {
+  snake.x=0;
+  snake.y=0;
+  snake.xSpeed=scale*1;
+  snake.ySpeed=0;
+  snake.total=0;
+  snake.tail=[];
+  document.querySelector('.heading').innerText='Snake Game';
+  var element=document.querySelector('.btn');
+  element.removeChild(btn);
+  start();
+}
--- snake.js
+++ snake.js
@@ -80,10 +80,11 @@ function Snake () {
   this.checkCollision=function () {
     for (var i = 0; i < this.tail.length; i++) {
       if(this.x===this.tail[i].x && this.y===this.tail[i].y){
-        this.total=0;
-        this.tail=[];
-        
+        //this.total=0;
+        //this.tail=[];
+        return true;
       }
     }
+    return false;
   }
 }
