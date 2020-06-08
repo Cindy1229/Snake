91b1f1da8bfd187caf157e09c15970948ed6a934
Cindy1229 <leticiashi3@gmail.com>
2020-06-08 14:09:18 -0400

    Added apple

--- draw.js
+++ draw.js
@@ -5,14 +5,23 @@ const rows = canvas.height / scale;
 const columns = canvas.width / scale;
 var snake;
 
+
 //Setup function
 (function setup() {
+  //Create snake and apple
   snake = new Snake();
+  apple = new Apple();
   snake.draw();
+
+  //Create apple in random spot
+  apple.pickLocation();
+  console.log(apple);
+
 
   //Redraw the canvas every 250 miliseconds
   window.setInterval(()=>{
     ctx.clearRect(0,0,canvas.width, canvas.height);
+    apple.draw();
     snake.update();
     snake.draw();
   }, 250)
